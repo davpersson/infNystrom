a25198514f66a18df7a54eef77aecce161707e08
Nicolas Boulle <nicolas.boulle1@gmail.com>
2024-02-27 15:44:35 +0000

    compare chol and tsvd

--- src/pretty_kernel.m
+++ src/pretty_kernel.m
@@ -6,5 +6,23 @@ l = 0.01;
 gaussian_01 = chebfun2(@(x,y) exp(-(x-y).^2/(2*l^2)));
 %%
 n = 100;
+
 Fn = nystrom_psd(K, gaussian_01, n);
-norm(K-Fn)
+fprintf("MT paper: %.2e\n", norm(K-Fn))
+
+Fn = nystrom_psd2(K, gaussian_01, n);
+fprintf("Truncated SVD: %.2e\n", norm(K-Fn))
+%%
+n = 100;
+MT = [];
+TSVD = [];
+for i = 1:10
+    sprintf("%d / 10",i)
+    Fn = nystrom_psd(K, gaussian_01, n);
+    MT = [MT, norm(K-Fn)];
+
+    Fn = nystrom_psd2(K, gaussian_01, n);
+    TSVD = [TSVD, norm(K-Fn)];
+end
+fprintf("MT paper: %.2e, TSVD: %.2e\n", mean(MT), mean(TSVD))
+
